1dcd9b91762da9335445c952b369e0dbf378e9ce
gy <gyonee@163.com>
2019-06-19 16:42:15 +0800

    test_01

---
+++ src/main/java/com/xmcc/spring_demo1/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.xmcc.spring_demo1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        log.info("hello,Slf4j");
+        return "hello";
+    }
+
+}
